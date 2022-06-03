6187bdc5fa537fa1149562bf313219817128f06d
Iviyan <Elephant-5@yandex.ru>
2022-06-03 12:41:37 +0300

    flashlight

--- app/src/main/java/com/iv/androidpw/MainActivity.java
+++ app/src/main/java/com/iv/androidpw/MainActivity.java
@@ -2,13 +2,45 @@ package com.iv.androidpw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    CameraManager camManager;
+    String cameraId = null;
+
+    boolean flashlight = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+
+        camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+            cameraId = camManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Ошибка доступа к фонарику", Toast.LENGTH_SHORT).show();
+        }
+
+        btn.setOnClickListener(view -> {
+            try {
+                camManager.setTorchMode(cameraId, flashlight = !flashlight);
+                btn.setText("Flashlight - " + (flashlight ? "off" : "on"));
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Ошибка включения фонарика", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
